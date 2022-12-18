779a29a001f7a5f0e9c8499fb16f2c5f47e8a806
Manuel Martinez <manmartgarc@gmail.com>
2022-12-17 19:18:19 -0500

    test: add basic test

---
+++ tests/test_geopull.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2022-12-17 07:13:45-05:00
+===============================================================================
+@filename:  test_geopull.py
+@author:    Manuel Martinez (manmart@uchicago.edu)
+@project:   geopull
+@purpose:   Test top-level geopull imoports
+===============================================================================
+"""
+# pylint: disable=import-outside-toplevel,missing-function-docstring
+# pylint: disable=unused-import
+
+
+def test_import_geopull():
+    import geopull  # noqa: F401
